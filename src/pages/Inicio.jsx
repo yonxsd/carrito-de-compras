584860398a838bd76724f0089310720106c5594f
Jonatan David Campos Barrera <gemeryonatan077@gmail.com>
2024-05-16 02:51:04 -0600

    modificado

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../css/Inicio.css"
+
+const Inicio = () => {
+  return (
+    <div className='Inic'>
+       <div className='inicop'>
+         <img src="" alt="" />
+         <h1>producto 1</h1>
+         <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla hic, dicta quos modi alias beatae amet esse, dolorum consectetur quam accusantium quo error obcaecati pariatur voluptate a cum tempora facilis?</h2>
+          <button >agregar</button>
+       </div>
+    </div>
+  )
+}
+
+export default Inicio
