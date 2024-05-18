f0d272258ac7b8d0da8e1ce74361088cc5ea8989
Jonatan David Campos Barrera <gemeryonatan077@gmail.com>
2024-05-18 04:32:48 -0600

    modificado

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -3,12 +3,46 @@ import "../css/Inicio.css"
 
 const Inicio = () => {
   return (
-    <div className='Inic'>
-       <div className='inicop'>
-         <img src="" alt="" />
-         <h1>producto 1</h1>
-         <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla hic, dicta quos modi alias beatae amet esse, dolorum consectetur quam accusantium quo error obcaecati pariatur voluptate a cum tempora facilis?</h2>
-          <button >agregar</button>
+    <div>
+       <div className='Inic'>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
+       </div>
+       <div className='Inic'>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
+          <div className='inicop'>
+            <img src="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" alt="" />
+            <h1>producto 1</h1>
+            <h2>Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric</h2>
+             <button className='but'>agregar</button>
+          </div>
        </div>
     </div>
   )
